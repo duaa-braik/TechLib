fd4db4b4cb49f0a2cdaa4233106ea221566af1fe
duaa-braik <duaa13braik@gmail.com>
2024-04-07 22:32:35 +0200

    Moved files to directories

--- Queries/PeakDays.sql
+++ Queries/PeakDays.sql
@@ -3,7 +3,7 @@
 	--From the data you have, determine the most 3 days in the week that have the most share of the loans and display the result of each
 	--day as a percentage of all loans. Sort the results from the highest percentage to the lowest percentage.
 	--(eg. 25.18% of the loans happen on Monday...)
-
+use TechLib
 with PeakDays as
 (
 	select top(3) DATENAME(WEEKDAY, l.DateBorrowed) as DayOfTheWeek,
@@ -11,8 +11,7 @@ with PeakDays as
 	from Loan l
 	group by DATENAME(WEEKDAY, l.DateBorrowed)
 	order by LoanPercentagePerDay desc
-)
-
+) 
 
 select DayOfTheWeek
 from PeakDays
